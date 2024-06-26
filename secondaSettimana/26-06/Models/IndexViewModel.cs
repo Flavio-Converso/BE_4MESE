a02a2120dad89d225663eda2aac4e975efc560ea
Flavio-Converso <fla.converso@gmail.com>
2024-06-26 17:03:25 +0200

    26-06

---
+++ secondaSettimana/26-06/Models/IndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace _26_06.Models
+{
+    public class IndexViewModel
+    {
+        public Persona Persona { get; set; }
+        public List<Sala> Sale { get; set; }
+        public Biglietto Biglietto { get; set; }
+
+        public IndexViewModel()
+        {
+            Sale = new List<Sala>
+            {
+                new Sala { NomeSala = "SalaEst", PostiTotali = 120 },
+                new Sala { NomeSala = "SalaSud", PostiTotali = 120 },
+                new Sala { NomeSala = "SalaNord", PostiTotali = 120 }
+            };
+            Biglietto = new Biglietto();
+        }
+    }
+}
---
+++ secondaSettimana/26-06/Models/Sala.cs
@@ -0,0 +1,12 @@
+﻿namespace _26_06.Models
+{
+    public class Sala
+    {
+        public string NomeSala { get; set; }
+        public int PostiTotali { get; set; }
+        public int PostiOccupati { get; set; }
+        public int PostiRidotti { get; set; }
+
+        public int PostiDisponibili => PostiTotali - PostiOccupati;
+    }
+}
